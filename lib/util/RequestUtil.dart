16d36bcde0f59b76c28e7b7b94cd595a378d2390
UHAhahhh <2873585250@qq.com>
2026-01-21 02:15:50 +0800

    Alter RequestUtil to a static method.

--- lib/util/RequestUtil.dart
+++ lib/util/RequestUtil.dart
@@ -1,65 +1,45 @@
 import 'package:dio/dio.dart';
 
-import '../configuration/GlobalConfiguration.dart';
-
 class RequestUtil {
+  final Dio _dio;
 
-  RequestUtil._();
-
-  static late final Dio _dio;
-
-  static bool _isInitialized = false;
-
-  static void init({
-    String? baseUrl,
-    int? timeout,
+  RequestUtil({
+    required String baseUrl,
+    int timeout = 10000,
     List<Interceptor>? interceptors,
-  }) {
-    if (_isInitialized) return;
-
-    final options = BaseOptions(
-      baseUrl: baseUrl ?? GlobalConfiguration.baseUrl,
-      connectTimeout: Duration(milliseconds: timeout ?? GlobalConfiguration.connectTimeout),
-      receiveTimeout: Duration(milliseconds: timeout ?? GlobalConfiguration.receiveTimeout),
-      contentType: 'application/json; charset=utf-8',
-    );
-
-    _dio = Dio(options);
-
-    if (interceptors != null && interceptors.isNotEmpty) {
+  }) : _dio = Dio(BaseOptions(
+    baseUrl: baseUrl,
+    connectTimeout: Duration(milliseconds: timeout),
+    receiveTimeout: Duration(milliseconds: timeout),
+    contentType: 'application/json; charset=utf-8',
+  )) {
+    if (interceptors != null) {
       _dio.interceptors.addAll(interceptors);
     }
-
-    _isInitialized = true;
   }
 
-  static Future<T?> request<T>(
+  Future<dynamic> request(
       String path, {
         String method = 'GET',
         dynamic data,
         Map<String, dynamic>? query,
-        Options? options,
       }) async {
-
-    assert(_isInitialized, "在使用 RequestUtil 之前必须先调用 init()");
-
     try {
       final response = await _dio.request(
         path,
         data: data,
         queryParameters: query,
-        options: options?.copyWith(method: method) ?? Options(method: method),
+        options: Options(method: method),
       );
-      return response.data as T?;
+      return response.data;
     } catch (e) {
-
       rethrow;
     }
   }
 
-  static Future<T?> get<T>(String path, {Map<String, dynamic>? query}) =>
-      request<T>(path, method: 'GET', query: query);
+  Future<dynamic> get(String path, {Map<String, dynamic>? query}) =>
+      request(path, method: 'GET', query: query);
 
-  static Future<T?> post<T>(String path, {dynamic data}) =>
-      request<T>(path, method: 'POST', data: data);
+  Future<dynamic> post(String path, {dynamic data}) =>
+      request(path, method: 'POST', data: data);
 }
